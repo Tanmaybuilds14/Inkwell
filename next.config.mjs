51fb70bd45d8c0534be86115644bf7b826c4e13c
Tanmaybuilds14 <sarvetanmay276@gmail.com>
2026-08-25 19:52:15 +0530

    Initial setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '2mb',
+    },
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.clerk.dev',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
